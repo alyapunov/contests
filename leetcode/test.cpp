14b265f179ba836bc67bf0db77821a07334f52a2
Aleksandr Lyapunov <alyapunov@tarantool.org>
2022-02-16 18:31:37 +0300

    strange

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -1,51 +1,77 @@
-#include <alya.h>
+#include <cassert>
+#include <iostream>
 #include <vector>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <climits>
 
 using namespace std;
 
-//  Definition for singly-linked list.
-struct ListNode {
-	int val;
-	ListNode *next;
-};
-
+constexpr size_t K = 7;
+constexpr size_t N = 1 << K;
 
 class Solution {
 public:
-	ListNode* reverseKGroup(ListNode* head, int k) {
+	uint32_t data[N];
+	int firstMissingPositive(const vector<int>& nums, int from, int count) {
+		size_t l = (count - 1) / 32 + 1;
+		for (size_t i = 0; i < l; i++)
+			data[i] = 0;
+		for (int i : nums) {
+			if (i >= from && i < int(32 * N) + from)
+				data[(i - from) / 32] |= 1u << ((i - from) % 32);
+		}
+		for (size_t i = 0; i < l; i++) {
+			if (data[i] != UINT32_MAX)
+				return from + i * 32 + __builtin_ctz(~data[i]);
+		}
+		return -1;
+	}
+	int firstMissingPositive(const vector<int>& nums) {
+		int from = 1;
+		if (nums.size() < 32 * N)
+			return firstMissingPositive(nums, from, nums.size());
+		assert(nums.size() < 32 * N * N);
+		size_t l = nums.size() / (32 * N) + 1;
+		for (size_t i = 0; i < l; i++)
+			data[i] = 0;
+		for (int i : nums) {
+			if (i >= from && i < int(32 * N * N) + from)
+				data[(i - from) / (32 * N)]++;
+		}
+		for (size_t i = 0; i < l; i++) {
+			if (data[i] != 32 * N)
+				return firstMissingPositive(nums, from + i * 32 * N, 32 * N);
+		}
+		return -1;
 	}
 };
 
 void
-check(std::initializer_list<int> orig, int k, std::initializer_list<int> res)
+check(const vector<int>& nums, int res)
 {
-	std::vector<ListNode> nodes(orig.size());
-	{
-		size_t i = 0;
-		for (int val : orig) {
-			auto &n = nodes[i++];
-			n.val = val;
-			n.next = i == orig.size() ? nullptr : &nodes[i];
-		}
-	}
-	Solution sol;
-	ListNode* r = sol.reverseKGroup(&nodes[0], k);
-	{
-		size_t i = 0;
-		for (int val : res) {
-			if (val != r->val)
-				abort();
-			r = r->next;
-		}
-		if (r != nullptr)
-			abort();
+	Solution s;
+	int my = s.firstMissingPositive(nums);
+	if (my != res) {
+		std::cout << "expected " << res << ", got " << my << "\n";
+		abort();
 	}
 }
 
 int main()
 {
-	check({1,2,3,4,5}, 2, {2,1,4,3,5});
-	check({1,2,3,4,5}, 3, {1,2,3,4,5});
-	check({1,2,3,4,5}, 1, {1,2,3,4,5});
-	check({1}, 1, {1});
+	check({1,2,0}, 3);
+	check({3,4,-1,1}, 2);
+	check({7,8,9,11,12}, 1);
+	std::vector<int> test;
+	int n = 500000;
+	test.reserve(n);
+	for (int i = 1; i <= n; i++)
+		test.push_back(i);
+	check(test, n + 1);
+	test.clear();
+	for (int i = 1; i <= n; i++)
+		test.push_back(i + 1);
+	check(test, 1);
 }
