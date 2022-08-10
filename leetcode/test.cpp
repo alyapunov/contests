730261f424e8bf293be88fb3472dba70894c478a
Aleksandr Lyapunov <alyapunov@tarantool.org>
2022-08-11 00:53:23 +0300

    my

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -3,13 +3,33 @@
 #include <iterator>
 #include <vector>
 
-using namespace std;
+int best[100000];
 
 class Solution {
 public:
-	std::vector<int> method(const std::vector<int>& data)
-	{
-		return data;
+	int maxProfit(const std::vector<int>& prices) {
+		best[0] = 0;
+		int m = prices[0];
+		int b = 0;
+		for (size_t i = 1; i < prices.size(); i++) {
+			if (b < prices[i] - m)
+				b = prices[i] - m;
+			if (m > prices[i])
+				m = prices[i];
+			best[i] = b;
+		}
+		m = prices.back();
+		b = 0;
+		int res = 0;
+		for (size_t i = prices.size() - 1; i-- != 0;) {
+			if (b < m - prices[i])
+				b = m - prices[i];
+			if (m < prices[i])
+				m = prices[i];
+			if (res < b + best[i])
+				res = b + best[i];
+		}
+		return res;
 	}
 };
 
@@ -26,10 +46,10 @@ std::ostream& operator<<(std::ostream& strm, const std::vector<T>& vec)
 }
 
 void
-check(const std::vector<int>& data, const std::vector<int>& expected)
+check(const std::vector<int>& data, int expected)
 {
 	Solution sol;
-	auto got = sol.method(data);
+	auto got = sol.maxProfit(data);
 	if (got != expected) {
 		std::cout << data
 			  << " : "
@@ -41,5 +61,11 @@ check(const std::vector<int>& data, const std::vector<int>& expected)
 
 int main()
 {
-	check({1, 2, 3, 4, 5}, {1, 2, 3, 4, 5});
+	check({3, 3, 5, 0, 0, 3, 1, 4}, 6);
+	check({1, 2, 3, 4, 5}, 4);
+	check({7, 6, 4, 3, 1}, 0);
+
+	check({1, 2}, 1);
+	check({10, 11, 1, 10}, 10);
+//	check({}, 0);
 }
