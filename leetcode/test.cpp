143fc4e68055a1b26ac276f3cd8dd8d7455ba94f
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-03-30 13:13:42 +0300

    good

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -3,14 +3,85 @@
 #include <iterator>
 #include <optional>
 #include <vector>
+#include <bitset>
 
-using namespace std;
+constexpr size_t MAX_LEN = 2000;
+
+template <size_t N = MAX_LEN>
+class PaliCheck {
+public:
+	void init(std::string_view s)
+	{
+		intptr_t n = s.size();
+		for (intptr_t i = 0; i < n; i++) {
+			intptr_t j;
+			for (j = 0;; j++) {
+				intptr_t k1 = i - j;
+				intptr_t k2 = i + j;
+				if (k1 < 0 || k2 >= n)
+					break;
+				if (s[k1] != s[k2])
+					break;
+				set(k1, k2 + 1, true);
+			}
+			for (;; j++) {
+				intptr_t k1 = i - j;
+				intptr_t k2 = i + j;
+				if (k1 < 0 || k2 >= n)
+					break;
+				set(k1, k2 + 1, false);
+			}
+			for (j = 0;; j++) {
+				intptr_t k1 = i - j;
+				intptr_t k2 = i + j + 1;
+				if (k1 < 0 || k2 >= n)
+					break;
+				if (s[k1] != s[k2])
+					break;
+				set(k1, k2 + 1, true);
+			}
+			for (;; j++) {
+				intptr_t k1 = i - j;
+				intptr_t k2 = i + j + 1;
+				if (k1 < 0 || k2 >= n)
+					break;
+				set(k1, k2 + 1, false);
+			}
+		}
+	}
+	bool test(size_t i, size_t j)
+	{
+		return bits.test(i * (N + 1) + j);
+	}
+private:
+	void set(size_t i, size_t j, bool value = true)
+	{
+		bits.set(i * (N + 1) + j, value);
+	}
+	std::bitset<(N + 1) * (N + 1)> bits;
+};
+
+PaliCheck p;
+
+int cache[MAX_LEN + 1];
 
 class Solution {
 public:
-	std::vector<int> method(const std::vector<int>& data)
-	{
-		return data;
+	int minCut(const std::string &s) {
+		cache[s.size()] = -1;
+		p.init(s);
+		for (size_t i = s.size() - 1; i + 1 > 0; i--) {
+			int m = MAX_LEN;
+			for (size_t j = i + 1; j <= s.size(); j++) {
+				if (!p.test(i, j))
+					continue;
+				int n = 1 + cache[j];
+				if (m > n)
+					m = n;
+			}
+			cache[i] = m;
+		}
+		return cache[0];
 	}
 };
 
@@ -36,10 +107,10 @@ std::ostream& operator<<(std::ostream& strm, const std::vector<T>& vec)
 }
 
 void
-check(const std::vector<int>& data, const std::vector<int>& expected)
+check(const std::string& data, int expected)
 {
 	Solution sol;
-	auto got = sol.method(data);
+	auto got = sol.minCut(data);
 	if (got != expected) {
 		std::cout << data
 			  << " : "
@@ -51,5 +122,9 @@ check(const std::vector<int>& data, const std::vector<int>& expected)
 
 int main()
 {
-	check({1, 2, 3, 4, 5}, {1, 2, 3, 4, 5});
+	check("a", 0);
+	check("ab", 1);
+	check("aab", 1);
+	check("abcbaabdba", 1);
+	check("abbaabdbaabeba", 2);
 }
