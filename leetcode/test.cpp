941975fd8995021c3ecd15a163b85a4b1ae87016
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-08-21 09:09:47 +0300

    leetcode: improve template

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -35,12 +35,41 @@ std::ostream& operator<<(std::ostream& strm, const std::vector<T>& vec)
 	return strm << ']';
 }
 
+template <class T>
+bool
+any_order_equal(const T& a, const T& b)
+{
+	return a == b;
+}
+
+template <class T>
+bool
+any_order_equal(const std::vector<T>& a, const std::vector<T>& b)
+{
+	if (a.size() != b.size())
+		return false;
+	for (size_t i = 0; i < a.size(); i++) {
+		const T& x = a[i];
+		size_t c1 = 0, c2 = 0;
+		for (const auto &y : a)
+			if (any_order_equal(x, y))
+				c1++;
+		for (const auto &y : b)
+			if (any_order_equal(x, y))
+				c2++;
+		if (c1 != c2)
+			return false;
+	}
+	return true;
+}
+
 void
 check(const std::vector<int>& data, const std::vector<int>& expected)
 {
 	Solution sol;
 	auto got = sol.method(data);
 	if (got != expected) {
+	//if (!any_order_equal(got, expected)) {
 		std::cout << data
 			  << " : "
 			  << "expected " << expected
