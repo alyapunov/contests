d3573d195ebb15947168686fad5199b090ca6afb
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-11-28 17:00:21 +0300

    Works

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -4,13 +4,75 @@
 #include <optional>
 #include <vector>
 
-using namespace std;
+//using namespace std;
+
+const char *L0[20] = {
+	" Zero", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine",
+	" Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen",
+};
+
+const char *L1[10] = {
+	" Nothing", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety",
+};
+
+const char *H[3] = {" Thousand", " Million", " Billion"};
 
 class Solution {
 public:
-	std::vector<int> method(const std::vector<int>& data)
+	void append(std::string& res, const char *str)
 	{
-		return data;
+		if (res.empty())
+			res = str + 1;
+		else
+			res += str;
+	}
+	void tripletToWord(std::string& res, int num)
+	{
+		if (num >= 100) {
+			int h = num / 100;
+			num = num % 100;
+			append(res, L0[h]);
+			append(res, " Hundred");
+		}
+		if (num == 0)
+			return;
+		if (num < 20) {
+			append(res, L0[num]);
+			return;
+		}
+		int h = num / 10;
+		int l = num % 10;
+		append(res, L1[h]);
+		if (l != 0)
+			append(res, L0[l]);
+	}
+
+	std::string numberToWords(int num)
+	{
+		if (num == 0)
+			return L0[0] + 1;
+
+		std::string res;
+
+		int div = 1;
+		for (size_t i = 0; i < sizeof(H) / sizeof(H[0]); i++)
+			div *= 1000;
+
+		for (size_t i = sizeof(H) / sizeof(H[0]); i-- != 0;) {
+			if (num >= div) {
+				int h = num / div;
+				num = num % div;
+				tripletToWord(res, h);
+				append(res, H[i]);
+			}
+			div /= 1000;
+		}
+
+		if (num != 0)
+			tripletToWord(res, num);
+
+		return res;
+
 	}
 };
 
@@ -64,10 +126,10 @@ any_order_equal(const std::vector<T>& a, const std::vector<T>& b)
 }
 
 void
-check(const std::vector<int>& data, const std::vector<int>& expected)
+check(int data, const std::string& expected)
 {
 	Solution sol;
-	auto got = sol.method(data);
+	auto got = sol.numberToWords(data);
 	if (got != expected) {
 	//if (!any_order_equal(got, expected)) {
 		std::cout << data
@@ -80,5 +142,8 @@ check(const std::vector<int>& data, const std::vector<int>& expected)
 
 int main()
 {
-	check({1, 2, 3, 4, 5}, {1, 2, 3, 4, 5});
+	check(0, "Zero");
+	check(123, "One Hundred Twenty Three");
+	check(12345, "Twelve Thousand Three Hundred Forty Five");
+	check(1234567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven");
 }
