51bf307b1554938de042419e4cd2c7dca9cb013b
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-12-01 15:28:11 +0300

    great

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -6,11 +6,105 @@
 
 using namespace std;
 
+enum op_t {
+	JUNCT,
+	MULT,
+	PLUS,
+	MINUS,
+};
+
+const char *op_str[] = {"", "*", "+", "-"};
+
+struct Applier {
+	long sum = 0;
+	long mul = 1;
+	long val = 0;
+	bool allowed = true;
+
+	void init(int dig)
+	{
+		val = dig;
+	}
+
+	void add(int op, int dig)
+	{
+		switch (op) {
+		case JUNCT:
+			allowed = allowed && (val != 0);
+			val = val * 10 + dig;
+			break;
+		case MULT:
+			mul *= val;
+			val = dig;
+			break;
+		case PLUS:
+			sum += mul * val;
+			mul = 1;
+			val = dig;
+			break;
+		case MINUS:
+			sum += mul * val;
+			mul = -1;
+			val = dig;
+			break;
+		default:
+			__builtin_unreachable();
+		};
+	}
+
+	long get() const
+	{
+		return sum + mul * val;
+	}
+};
+
 class Solution {
 public:
-	std::vector<int> method(const std::vector<int>& data)
-	{
-		return data;
+	std::vector<std::string> addOperators(const std::string& num, int trg) {
+		int digits[10];
+		size_t n = num.size();
+		for (size_t i = 0; i < n; i++)
+			digits[i] = num[i] - '0';
+
+		int ops[10] = {-1};
+
+		Applier app[10];
+		app[0].init(digits[0]);
+		for (size_t i = 1; i < n; i++) {
+			app[i] = app[i - 1];
+			app[i].add(ops[i], digits[i]);
+		}
+
+		std::vector<std::string> res;
+		int prev = 0;
+		for (int var = 0; var < (1 << ((n - 1) * 2)); var++) {
+			int k = n;
+			int v = var;
+			while (v != prev) {
+				k--;
+				ops[k] = v % 4;
+				v /= 4;
+				prev /= 4;
+			}
+			prev = var;
+
+			for (size_t i = k; i < n; i++) {
+				app[i] = app[i - 1];
+				app[i].add(ops[i], digits[i]);
+			}
+
+			if (app[n - 1].allowed && app[n - 1].get() == trg) {
+				std::string str;
+				str += char('0' + digits[0]);
+				for (size_t i = 1; i < n; i++) {
+					str += op_str[ops[i]];
+					str += char('0' + digits[i]);
+				}
+				res.emplace_back(std::move(str));
+			}
+		}
+
+		return res;
 	}
 };
 
@@ -64,13 +158,13 @@ any_order_equal(const std::vector<T>& a, const std::vector<T>& b)
 }
 
 void
-check(const std::vector<int>& data, const std::vector<int>& expected)
+check(const std::string& num, int target, const std::vector<std::string>& expected)
 {
 	Solution sol;
-	auto got = sol.method(data);
-	if (got != expected) {
-	//if (!any_order_equal(got, expected)) {
-		std::cout << data
+	auto got = sol.addOperators(num, target);
+	//if (got != expected) {
+	if (!any_order_equal(got, expected)) {
+		std::cout << num << " " << target
 			  << " : "
 			  << "expected " << expected
 			  << " got " << got << std::endl;
@@ -80,5 +174,34 @@ check(const std::vector<int>& data, const std::vector<int>& expected)
 
 int main()
 {
-	check({1, 2, 3, 4, 5}, {1, 2, 3, 4, 5});
+//	constexpr int N = 3;
+//	int arr[N + 1] = {1, 2, 3, 4};
+//	for (int i = 0; i < 4; i++) {
+//		for (int j = 0; j < 4; j++) {
+//			for (int k = 0; k < 4; k++) {
+//				int ops[N] = {i, j, k};
+//				Applier a;
+//				a.init(arr[0]);
+//				for (int ai = 0; ai < N; ai++)
+//					a.add(ops[ai], arr[ai + 1]);
+//
+//				std::cout << char('0' + arr[0]);
+//				for (int ai = 0; ai < N; ai++)
+//					std::cout << op_str[ops[ai]] << char('0' + arr[ai + 1]);
+//				std::cout << " = " << a.get() << std::endl;
+//			}
+//		}
+//	}
+//	return 0;
+
+	check("1", 1, {"1"});
+	check("123", 6, {"1*2*3","1+2+3"});
+	check("105", 5, {"1*0+5","10-5"});
+	check("232", 8, {"2*3+2","2+3*2"});
+	check("1333", 28, {"1+3*3*3"});
+	check("1333", -26, {"1-3*3*3"});
+	check("951", 5, {"9-5+1"});
+	check("9511", 5, {"9-5+1*1", "9-5*1+1"});
+	check("3456237490", 9191, {});
+	check("00", 0, {"0*0","0+0","0-0"});
 }
