b582f06b7533a8708489eb03b11b9ec51f606308
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-12-28 18:49:02 +0300

    Even worse

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -7,76 +7,126 @@
 
 //using namespace std;
 
+using number_t = uint32_t;
+using count_t = uint16_t;
+constexpr size_t MAX_BASE = 16;
+
+template <number_t N, bool IS_LAST = (N <= MAX_BASE)>
+struct Cont {
+	static constexpr size_t ct_pow(size_t pow, size_t base)
+	{
+		return pow > 0 ? base * ct_pow(pow - 1, base) : 1;
+	}
+
+	static constexpr size_t ct_log(size_t a, size_t base)
+	{
+		return a >= base ? 1 + ct_log(a / base, base) : 0;
+	}
+
+	static constexpr number_t NEXT = ct_pow(ct_log(N - 1, MAX_BASE), MAX_BASE);
+	static constexpr size_t SIZE = (N - 1) / NEXT + 1;
+
+	count_t counts[SIZE] = {};
+	Cont<NEXT> next[SIZE];
+
+	void reset()
+	{
+		for (size_t i = 0; i < SIZE; i++) {
+			if (counts[i] > 0) {
+				next[i].reset();
+				counts[i] = 0;
+			}
+		}
+	}
+	void add(number_t num)
+	{
+		counts[num / NEXT]++;
+		next[num / NEXT].add(num % NEXT);
+	}
+	number_t get(count_t pos)
+	{
+		number_t i = 0;
+		while (pos >= counts[i])
+			pos -= counts[i++];
+		return i * NEXT + next[i].get(pos);
+	}
+	std::pair<number_t, number_t> get2(count_t pos)
+	{
+		number_t i = 0;
+		while (pos >= counts[i])
+			pos -= counts[i++];
+		if (pos + 1 < counts[i]) {
+			std::pair<number_t, number_t> tmp = next[i].get2(pos);
+			return {i * NEXT + tmp.first, i * NEXT + tmp.second};
+		}
+		number_t first = i * NEXT + next[i].get(pos);
+		pos++;
+		do {
+			pos -= counts[i++];
+		} while (pos >= counts[i]);
+		number_t second = i * NEXT + next[i].get(pos);
+		return {first, second};
+	}
+};
+
+template <number_t N>
+struct Cont<N, true> {
+	static constexpr size_t SIZE = N;
+	count_t counts[SIZE];
+
+	void reset()
+	{
+		for (size_t i = 0; i < SIZE; i++)
+			counts[i] = 0;
+	}
+	void add(number_t num)
+	{
+		counts[num]++;
+	}
+	number_t get(count_t pos)
+	{
+		number_t i = 0;
+		while (pos >= counts[i])
+			pos -= counts[i++];
+		return i;
+	}
+	std::pair<number_t, number_t> get2(count_t pos)
+	{
+		number_t i = 0;
+		while (pos >= counts[i])
+			pos -= counts[i++];
+		number_t j = i;
+		pos++;
+		while (pos >= counts[j])
+			pos -= counts[j++];
+		return {i, j};
+	}
+};
+
+Cont<200001> cont;
+
 class MedianFinder {
 public:
-	std::vector<int> a, b;
+	uint16_t total = 0;
 
 	MedianFinder()
 	{
-
+		cont.reset();
 	}
 
-	void push_a(int num)
-	{
-		a.push_back(num);
-		std::push_heap(a.begin(), a.end());
-	}
-
-	int pop_a()
-	{
-		std::pop_heap(a.begin(), a.end());
-		int res = a.back();
-		a.pop_back();
-		return res;
-	}
-
-	void push_b(int num)
-	{
-		b.push_back(num);
-		std::push_heap(b.begin(), b.end(), std::greater<int>{});
-	}
-
-	int pop_b()
-	{
-		std::pop_heap(b.begin(), b.end(), std::greater<int>{});
-		int res = b.back();
-		b.pop_back();
-		return res;
-	}
 
 	void addNum(int num)
 	{
-		if (a.empty() && b.empty())
-			return a.push_back(num);
-
-		if (b.empty()) {
-			if (num < a[0])
-				std::swap(num, a[0]);
-			return b.push_back(num);
-		}
-
-		if (num < a[0]) {
-			if (a.size() > b.size())
-				push_b(pop_a());
-			push_a(num);
-		} else if (num > b[0]) {
-			if (b.size() >= a.size())
-				push_a(pop_b());
-			push_b(num);
-		} else {
-			if (a.size() > b.size())
-				push_b(num);
-			else
-				push_a(num);
-		}
+		cont.add(num + 100000);
+		total++;
 	}
 
 	double findMedian()
 	{
-		if (a.size() > b.size())
-			return a[0];
-		else
-			return (double(a[0]) + double(b[0])) / 2;
+		if (total % 2 == 1)
+			return double(cont.get(total / 2)) - 100000;
+		auto [a, b] = cont.get2(total / 2 - 1);
+		return (double(a) + double(b)) / 2 - 100000;
 	}
 };
 
@@ -150,4 +200,27 @@ int main()
 	check(1.5);
 	finder.addNum(3);
 	check(2);
+	for (size_t k = 0; k < 100; k ++)
+	{
+		MedianFinder perf;
+		double res = 0;
+		for (size_t i = 0; i < 50000; i++) {
+			int x = rand() % 200000 - 100000;
+			perf.addNum(x);
+			res += perf.findMedian();
+		}
+		std::cout << res << std::endl;
+	}
+	for (size_t k = 0; k < 100; k ++)
+	{
+		MedianFinder perf;
+		double res = 0;
+		for (size_t i = 0; i < 50000; i++) {
+			int x = i % 200000 - 100000;
+			perf.addNum(x);
+			res += perf.findMedian();
+		}
+		std::cout << res << std::endl;
+	}
+
 }
